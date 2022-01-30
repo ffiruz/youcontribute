ac56f1b9dd58b03f68bc84dca4bae8357107fdef
ferdi <ferdi.firuz@softtech.com.tr>
2022-01-30 16:42:25 +0300

    first commit

---
+++ src/test/java/com/ferdi/youcontribute/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferdi.youcontribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
